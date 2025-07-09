61543b44ba452792c9452dd5d8a3e92782f7b22a
GuillBuj <guillaume.bujon@gmail.com>
2025-07-09 11:27:09 +0200

    mini-correction test

--- src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -87,7 +87,7 @@ public class TestPerformance {
 		allUsers = tourGuideService.getAllUsers();
 		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
 
-		allUsers.forEach(u -> rewardsService.calculateRewardsAsync(u));
+		//allUsers.forEach(u -> rewardsService.calculateRewardsAsync(u));
 
 
 		//bloc ajouté au test pour attendre et éviter un assert raté par la suite
